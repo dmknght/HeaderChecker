0fcf6923a8c7358e6c97c3a262952c108109fbd7
dmknght <dmknght@parrotsec.org>
2020-01-28 01:02:53 +0700

    Upgrade module

--- cores/base_module.py
+++ cores/base_module.py
@@ -3,9 +3,19 @@ from cores import events
 
 class CoreModule(object):
     def __init__(self):
+        """
+        :self.name: Name of the header value
+        :self.url: URL of target (for programming logic only)
+        :self.data: Header response of target (programming logic onlY)
+        :self.suggestions: List of values should be used by default
+        :self.reference: URL of doc
+        :self.alert_missing_protocol: List of protocol (http, https)
+            the header should be set
+        """
         self.name = "Base Module name"
         self.url = ""
         self.data = ""
+        self.suggestions = []
         self.description = ""
         self.reference = ""
         self.alert_missing_protocol = []
@@ -14,8 +24,20 @@ class CoreModule(object):
         self.url = url
         self.data = data
 
+    def check_values(self):
+        status = False
+        for suggest in self.suggestions:
+            if suggest == self.data[self.name]:
+                status = True
+                break
+        if status == False:
+            pass
+            # TODO improve warning, error or warn with this
+
     def analysis(self):
-        if self.url.split("://")[0] in self.alert_missing_protocol:
-            events.data_error("{} is missing".format(self.name))
-            pass # TODO handle alert missing here
-        # TODO handle value here
+        if self.alert_missing_protocol:
+            if self.url.split("://")[0] in self.alert_missing_protocol:
+                events.data_error("{} is missing".format(self.name))
+                # No value to check. We don't have to validate values
+                return True
+        self.check_values()
--- modules/xss_protection.py
+++ modules/xss_protection.py
@@ -3,8 +3,12 @@ from cores import base_module
 
 class Action(base_module.CoreModule):
     def __init__(self):
-        super().__init__()
         self.name = "X-XSS-Protection"
         self.description = "Enable XSS filter in client's browser"
+        self.suggestions = [
+            "X-XSS-Protection: 1",
+            "X-XSS-Protection: 1; mode=block",
+            # "X-XSS-Protection: 1; report=<reporting-uri>", # TODO handle report
+        ]
         self.reference = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection"
         self.alert_missing_protocol = ["http", "https"]
