2ed2092144c4ffbb443b4628d47ebf246d59f14c
dmknght <dmknght@parrotsec.org>
2020-01-28 11:41:00 +0700

    Use value instead of full header value for suggestions

--- modules/frame_options.py
+++ modules/frame_options.py
@@ -6,8 +6,8 @@ class Action(base_module.CoreModule):
         self.name = "X-Frame-Options"
         self.description = "Avoid Clickjacking attack by ensuring that their content is not embedded into other sites"
         self.suggestions = [
-            "X-Frame-Options: deny",  # Block all frame
-            "X-Frame-Options: sameorigin",
+            "deny",  # Block all frame
+            "sameorigin",
             # "X-Frame-Options: allow-from uri", # TODO handle uri
         ]
         self.reference = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options"
--- modules/xss_protection.py
+++ modules/xss_protection.py
@@ -6,8 +6,8 @@ class Action(base_module.CoreModule):
         self.name = "X-XSS-Protection"
         self.description = "Enable XSS filter in client's browser"
         self.suggestions = [
-            "X-XSS-Protection: 1",
-            "X-XSS-Protection: 1; mode=block",
+            "1",
+            "1; mode=block",
             # "X-XSS-Protection: 1; report=<reporting-uri>", # TODO handle report
         ]
         self.reference = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection"
